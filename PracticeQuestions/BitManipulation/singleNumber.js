e014aa8b6e21259433ccf6fc811d20e5c2d99009
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2022-03-31 07:40:22 +0530

    Solution to find unique number in an array

---
+++ PracticeQuestions/BitManipulation/singleNumber.js
@@ -0,0 +1,98 @@
+/*
+Q1. Single Number
+
+Problem Description
+Given an array of integers A, every element appears twice except for one. Find that integer that occurs once.
+
+NOTE: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+
+
+Problem Constraints
+2 <= |A| <= 2000000
+
+0 <= A[i] <= INTMAX
+
+
+
+Input Format
+The first and only argument of input contains an integer array A.
+
+
+
+Output Format
+Return a single integer denoting the single element.
+
+
+
+Example Input
+Input 1:
+
+ A = [1, 2, 2, 3, 1]
+Input 2:
+
+ A = [1, 2, 2]
+
+
+Example Output
+Output 1:
+
+ 3
+Output 2:
+
+ 1
+
+
+Example Explanation
+Explanation 1:
+
+3 occurs once.
+Explanation 2:
+
+1 occurs once.
+*/
+
+// Solution 1, using bitwise operation XOR (^)
+//  TC - O(n)
+//  SC - O(1)
+
+function singleNumber(A){
+    let result = 0
+    for(let i=0; i<A.length; i++){
+        result  = result ^ A[i]
+    }
+return Number(result)
+}
+
+const A = [ 723, 256, 668, 
+    723, 140, 360, 597, 233, 128, 845, 737, 804, 986, 701, 906, 512, 845, 510, 510, 
+    227, 430, 701, 366, 946, 464, 619, 946, 627, 209, 771, 424, 555, 959, 711, 530, 937, 716, 261, 505, 658, 706, 140, 511, 277, 396, 233, 819, 196, 475, 906, 583, 261, 147, 658, 517, 197, 196, 702, 944, 711, 128, 555, 149, 483, 530, 291, 716, 258, 430, 464, 601, 749, 149, 415, 802, 573, 627, 771, 660, 601, 360, 986, 291, 51, 415, 51, 227, 258, 937, 366, 923, 669, 33, 517, 417, 702, 475, 706, 110, 417, 275, 804, 500, 473, 746, 973, 669, 275, 973, 147, 817, 657, 277, 923, 144, 660, 197, 511, 793, 893, 944, 505, 322, 817, 586, 512, 322, 668, 33, 424, 962, 597, 144, 746, 345, 753, 345, 269, 819, 483, 368, 802, 573, 962, 583, 615, 208, 209, 269, 749, 256, 657, 619, 893, 959, 473, 753, 299, 396, 299, 500, 368, 586, 110, 793, 737, 615 
+]
+
+console.log(singleNumber(A))
+
+// Solution 2
+// TC - O(N)
+// SC - O(N)
+function singleNumber2(A) {
+    let map = {}
+    for(let i=0; i<A.length; i++) {
+        if(map[A[i]]) {
+            map[A[i]]-=1
+        } else {
+            map[A[i]] = 1
+        }
+    }
+    let keys = Object.keys(map)
+    for(let i=0; i<Object.keys(map).length; i++) {
+        if(map[keys[i]] === 1) return keys[i]
+    }
+}
+
+const B = [ 723, 256, 668, 
+    723, 140, 360, 597, 233, 128, 845, 737, 804, 986, 701, 906, 512, 845, 510, 510, 
+    227, 430, 701, 366, 946, 464, 619, 946, 627, 209, 771, 424, 555, 959, 711, 530, 937, 716, 261, 505, 658, 706, 140, 511, 277, 396, 233, 819, 196, 475, 906, 583, 261, 147, 658, 517, 197, 196, 702, 944, 711, 128, 555, 149, 483, 530, 291, 716, 258, 430, 464, 601, 749, 149, 415, 802, 573, 627, 771, 660, 601, 360, 986, 291, 51, 415, 51, 227, 258, 937, 366, 923, 669, 33, 517, 417, 702, 475, 706, 110, 417, 275, 804, 500, 473, 746, 973, 669, 275, 973, 147, 817, 657, 277, 923, 144, 660, 197, 511, 793, 893, 944, 505, 322, 817, 586, 512, 322, 668, 33, 424, 962, 597, 144, 746, 345, 753, 345, 269, 819, 483, 368, 802, 573, 962, 583, 615, 208, 209, 269, 749, 256, 657, 619, 893, 959, 473, 753, 299, 396, 299, 500, 368, 586, 110, 793, 737, 615 
+]
+
+console.log(singleNumber2(B))
+
