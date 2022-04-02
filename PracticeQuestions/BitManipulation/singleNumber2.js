39df6fba847193c554c29a9a6580fba777efb78c
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2022-04-02 15:21:24 +0530

    Solution to find unique number in an array

---
+++ PracticeQuestions/BitManipulation/singleNumber2.js
@@ -0,0 +1,74 @@
+/*
+Single Number II
+
+Problem Description
+Given an array of integers, every element appears thrice except for one, which occurs once.
+
+Find that element that does not appear thrice.
+
+NOTE: Your algorithm should have a linear runtime complexity.
+
+Could you implement it without using extra memory?
+
+
+
+
+Problem Constraints
+2 <= A <= 5*106
+
+0 <= A <= INTMAX
+
+
+
+Input Format
+First and only argument of input contains an integer array A.
+
+
+
+Output Format
+Return a single integer.
+
+
+
+Example Input
+Input 1:
+
+ A = [1, 2, 4, 3, 3, 2, 2, 3, 1, 1]
+Input 2:
+
+ A = [0, 0, 0, 1]
+
+
+Example Output
+Output 1:
+
+ 4
+Output 2:
+
+ 1
+
+
+Example Explanation
+Explanation 1:
+
+ 4 occurs exactly once in Input 1.
+ 1 occurs exactly once in Input 2.
+*/
+function singleNumber2(A){
+    let ans = 0
+    for(let b=0; b<5; b++) {
+        let count = 0
+        for(let i = 0; i<A.length; i++) {
+            count += (A[i] >> b) & 1
+        }
+        if(count % 3 === 1) {
+            ans = (1 << b)
+        }
+    }
+    return ans
+}
+
+
+const A = [2, 4, 3, 3, 2, 2, 3, 5, 5, 5]
+
+console.log(singleNumber2(A))
