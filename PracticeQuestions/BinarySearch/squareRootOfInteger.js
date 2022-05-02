27cbd0f21cc0f83bfc28a335e51b86451997ebbe
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2022-05-02 10:57:52 +0530

    Binary search solution to find out square root of a number

---
+++ PracticeQuestions/BinarySearch/squareRootOfInteger.js
@@ -0,0 +1,71 @@
+/*
+Square Root of Integer
+
+Problem Description
+Given an integer A.
+
+Compute and return the square root of A.
+
+If A is not a perfect square, return floor(sqrt(A)).
+
+DO NOT USE SQRT FUNCTION FROM THE STANDARD LIBRARY.
+
+NOTE: Do not use the sqrt function from the standard library. Users are expected to solve this in O(log(A)) time.
+
+
+
+Problem Constraints
+0 <= A <= 1010
+
+
+
+Input Format
+The first and only argument given is the integer A.
+
+
+
+Output Format
+Return floor(sqrt(A))
+
+
+
+Example Input
+Input 1:
+
+ 11
+Input 2:
+
+ 9
+
+
+Example Output
+Output 1:
+
+ 3
+Output 2:
+
+ 3
+
+
+Example Explanation
+Explanation:
+
+ When A = 11 , square root of A = 3.316. It is not a perfect square so we return the floor which is 3.
+ When A = 9 which is a perfect square of 3, so we return 3.
+*/
+
+function sqrt(A) {
+  let L = 0;
+  let R = A;
+  while (L <= R) {
+    let mid = Math.floor((L + R) / 2);
+    let sqr = mid * mid;
+    if (sqr <= A && (mid + 1) * (mid + 1) > A) return mid;
+    if (sqr > A) R = mid - 1;
+    else L = mid + 1;
+  }
+}
+
+const A = 0;
+
+console.log(sqrt(A));
