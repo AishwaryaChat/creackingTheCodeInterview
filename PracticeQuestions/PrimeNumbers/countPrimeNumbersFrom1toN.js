33baf04237643ee2694967e026a59b3b1e07bee0
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2022-05-14 18:09:04 +0530

    Solution to find  prime numbers from 1to N

---
+++ PracticeQuestions/PrimeNumbers/countPrimeNumbersFrom1toN.js
@@ -0,0 +1,20 @@
+/*
+Given an integer N find the number of Prime Numbers from 1 to N
+*/
+
+function solve(N) {
+    let P = Array(N).fill(true)
+    P[0] = false 
+    P[1] = false
+    for(let i=2; i*i<=N; i++) {
+        if(!P[i]) continue
+        for(let j=i*i; j<=N; j+= i) {
+            P[j] = false
+        }
+    }
+    return P.map((p, i) => p ? i : p)
+    .filter(p => p)
+}
+
+const N = 18
+console.log(solve(N))
