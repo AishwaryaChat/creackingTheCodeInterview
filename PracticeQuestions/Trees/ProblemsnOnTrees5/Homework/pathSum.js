d4d97388a45908b25e15bb75431f13d1c771a382
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-01-28 17:40:51 +0530

    Solution to check if path sum in tree

---
+++ PracticeQuestions/Trees/ProblemsnOnTrees5/Homework/pathSum.js
@@ -0,0 +1,68 @@
+// Path Sum
+
+// Problem Description
+// Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+// Problem Constraints
+// 1 <= number of nodes <= 10^5
+
+// -100000 <= B, value of nodes <= 100000
+
+// Input Format
+// First argument is a root node of the binary tree, A.
+
+// Second argument is an integer B denoting the sum.
+
+// Output Format
+// Return 1, if there exist root-to-leaf path such that adding up all the values along the path equals the given sum. Else, return 0.
+
+// Example Input
+// Input 1:
+
+//  Tree:    5
+//          / \
+//         4   8
+//        /   / \
+//       11  13  4
+//      /  \      \
+//     7    2      1
+
+//  B = 22
+// Input 2:
+
+//  Tree:    5
+//          / \
+//         4   8
+//        /   / \
+//      -11 -13  4
+
+//  B = -1
+
+// Example Output
+// Output 1:
+
+//  1
+// Output 2:
+
+//  0
+
+// Example Explanation
+// Explanation 1:
+
+//  There exist a root-to-leaf path 5 -> 4 -> 11 -> 2 which has sum 22. So, return 1.
+// Explanation 2:
+
+//  There is no path which has sum -1.
+
+// TC - O(N)
+// SC - O(H) - stack space
+function checkForRoot(root, k) {
+  if (root === null) return 0;
+  k = k - root.data;
+  if (k === 0 && root.left === null && root.right === null) return 1;
+  return checkForRoot(root.left, k) || checkForRoot(root.right, k);
+}
+
+function solve(root, k) {
+  return checkForRoot(root, k);
+}
