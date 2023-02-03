7d38048e91f9c9626cb5e7d60fa4650e19c4cff8
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-02-03 15:18:19 +0530

    Solution to find cost of climbing stairs

---
+++ Neetcode/DynamicProgramming/minimumCostOfClimbingStairs.js
@@ -0,0 +1,88 @@
+// Min Cost Climbing Stairs
+// Easy
+
+// Companies
+// Amazon
+// Apple
+// Bloomberg
+// You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+// You can either start from the step with index 0, or the step with index 1.
+
+// Return the minimum cost to reach the top of the floor.
+
+// Example 1:
+
+// Input: cost = [10,15,20]
+// Output: 15
+// Explanation: You will start at index 1.
+// - Pay 15 and climb two steps to reach the top.
+// The total cost is 15.
+// Example 2:
+
+// Input: cost = [1,100,1,1,1,100,1,1,100,1]
+// Output: 6
+// Explanation: You will start at index 0.
+// - Pay 1 and climb two steps to reach index 2.
+// - Pay 1 and climb two steps to reach index 4.
+// - Pay 1 and climb two steps to reach index 6.
+// - Pay 1 and climb one step to reach index 7.
+// - Pay 1 and climb two steps to reach index 9.
+// - Pay 1 and climb one step to reach the top.
+// The total cost is 6.
+
+// Constraints:
+
+// 2 <= cost.length <= 1000
+// 0 <= cost[i] <= 999
+
+// TC - O((N)
+// SC - O(N) - dp space + stack space
+function findCost(cost, dp, n) {
+  if (n < 2) return dp[n];
+  if (dp[n] !== undefined) return dp[n];
+  dp[n] = Math.min(
+    cost[n - 1] + findCost(cost, dp, n - 1),
+    cost[n - 2] + findCost(cost, dp, n - 2)
+  );
+  return dp[n];
+}
+
+function solveRecursive(cost) {
+  // cost to reach 0 and 1 is 0
+  let dp = [0, 0];
+  const ans = findCost(cost, dp, cost.length);
+  return ans;
+}
+
+// TC - O(N)
+// SC - O(N), dp space
+function solveIterative(cost) {
+    let dp = [0,0]
+    for (let i = 2; i <= cost.length; i++) {
+      dp[i] = Math.min(cost[i - 1] + dp[i-1], cost[i - 2] + dp[i-2]);
+    }
+    return dp[cost.length];
+  }
+
+// TC - O(N)
+// SC - O(1)
+function solveIterativeSpaceOptimised(cost) {
+  let dp = 0;
+  let dp0 = 0;
+  let dp1 = 0;
+  for (let i = 2; i <= cost.length; i++) {
+    dp = Math.min(cost[i - 1] + dp1, cost[i - 2] + dp0);
+    dp0 = dp1;
+    dp1 = dp;
+  }
+  return dp;
+}
+
+// Input: cost = [10,15,20]
+// Output: 15
+
+Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
+// Output: 6
+
+console.log(solveIterative(cost));
