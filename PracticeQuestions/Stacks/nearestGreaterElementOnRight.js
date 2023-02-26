38768d9c17280a9984a0013538a00348e4a7725e
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-02-26 13:01:05 +0530

    Solution for sum of maximum - minimum of all subarrays

--- PracticeQuestions/Stacks/nearestGreaterElementOnRight.js
+++ PracticeQuestions/Stacks/nearestGreaterElementOnRight.js
@@ -1,18 +1,19 @@
 const Stack = require("./stack");
 
 function solve(A) {
-    let ans = [];
-    let st = new Stack();
-    for (let i = A.length-1; i >= 0; i--) {
-        while(!st.isEmpty() && A[st.peak()] <= A[i]) st.pop()
-        if(st.isEmpty())  ans[i] = -1
-        else ans[i] = st.peak()
-        st.push(i)
-    }
-    return ans
+  let ans = [];
+  let st = new Stack();
+  const n = A.length;
+  for (let i = n - 1; i >= 0; i--) {
+    while (!st.isEmpty() && A[st.peak()] <= A[i]) st.pop();
+    if (st.isEmpty()) ans[i] = n;
+    else ans[i] = st.peak();
+    st.push(i);
+  }
+  return ans;
 }
 
 const A = [4, 5, 2, 10, 8];
-console.log(solve(A));
+// console.log(solve(A));
 
 module.exports = solve;
--- PracticeQuestions/Stacks/nearestSmallerElementOnRight.js
+++ PracticeQuestions/Stacks/nearestSmallerElementOnRight.js
@@ -2,14 +2,12 @@ const Stack = require("./stack");
 
 function solve(A) {
   let ans = [];
+  const n = A.length;
   let st = new Stack();
-  for (let i = A.length - 1; i >= 0; i--) {
+  for (let i = n - 1; i >= 0; i--) {
     while (!st.isEmpty() && A[st.peak()] >= A[i]) st.pop();
-    if (st.isEmpty()) {
-      ans[i] = -1;
-    } else {
-      ans[i] = st.peak();
-    }
+    if (st.isEmpty()) ans[i] = n;
+    else ans[i] = st.peak();
     st.push(i);
   }
   return ans;
