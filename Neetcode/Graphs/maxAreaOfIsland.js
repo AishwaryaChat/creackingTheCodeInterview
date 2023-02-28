31e1c2e6f856f6841e96dc0c2957316817743bc2
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-02-28 19:20:57 +0530

    Calculate max area of island

---
+++ Neetcode/Graphs/maxAreaOfIsland.js
@@ -0,0 +1,78 @@
+// Max Area of Island
+// Medium
+// company
+// Dropbox
+// Amazon
+// Bloomberg
+// You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+// The area of an island is the number of cells with a value 1 in the island.
+
+// Return the maximum area of an island in grid. If there is no island, return 0.
+
+// Example 1:
+
+// Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+// Output: 6
+// Explanation: The answer is not 11, because the island must be connected 4-directionally.
+// Example 2:
+
+// Input: grid = [[0,0,0,0,0,0,0,0]]
+// Output: 0
+
+// Constraints:
+
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 50
+// grid[i][j] is either 0 or 1.
+
+let cx = [-1, 0, 1, 0];
+let cy = [0, 1, 0, -1];
+
+function dfs(grid, x, y, n, m, ans) {
+  for (let i = 0; i < cx.length; i++) {
+    const k = x + cx[i];
+    const l = y + cy[i];
+    if (k >= 0 && k < n && l >= 0 && l < m && grid[k][l] === 1) {
+      grid[k][l] = 0;
+      ans = dfs(grid, k, l, n, m, ans + 1);
+    }
+  }
+  return ans;
+}
+
+// TC - O(N*M)
+// SC - O(N*M)
+function solve(grid) {
+  const n = grid.length;
+  const m = grid[0].length;
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (grid[i][j] === 1) {
+        grid[i][j] = 0;
+        const midAns = dfs(grid, i, j, n, m, 1);
+        ans = Math.max(ans, midAns);
+      }
+    }
+  }
+  return ans;
+}
+
+const grid = [
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+];
+// Output: 6
+
+// const grid = [[0,0,0,0,0,0,0,0]]
+// Output: 0
+
+console.log(solve(grid));
