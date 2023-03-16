c119b7d56013aee9347464bd180db0f401b56290
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-03-16 16:29:20 +0530

    Solution to get unique paths in matrix

---
+++ companies/google/uniqueuePaths.js
@@ -0,0 +1,54 @@
+// Unique Paths
+// Medium
+// company
+// Bloomberg
+// Amazon
+// Google
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+// Example 1:
+
+// Input: m = 3, n = 7
+// Output: 28
+// Example 2:
+
+// Input: m = 3, n = 2
+// Output: 3
+// Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+// 1. Right -> Down -> Down
+// 2. Down -> Down -> Right
+// 3. Down -> Right -> Down
+
+// Constraints:
+
+// 1 <= m, n <= 100
+
+// TC - O(N^2)
+// SC - O(N^2)
+function solve(m, n) {
+  const dp = new Array(m).fill().map(() => new Array(n).fill(0));
+  dp[0][0] = 1;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i == 0 && j === 0) dp[i][j] = 1;
+      else if (i === 0) dp[i][j] = dp[i][j - 1];
+      else if (j === 0) dp[i][j] = dp[i - 1][j];
+      else dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+    }
+  }
+  return dp[m - 1][n - 1];
+}
+
+const m = 3;
+const n = 7;
+// Output: 28
+
+// const m = 3
+// const n = 2
+// Output: 3
+
+console.log(solve(m, n));
