a69296b31a71f522019880dad216cb0c9afb6769
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-03-16 15:55:50 +0530

    Solution for house robber

--- companies/google/houseRobber.js
+++ companies/google/houseRobber.js
@@ -30,40 +30,43 @@
 // TC - O(N)
 // SC - O(N)
 function solve(nums) {
-    let dp = []
-    dp[0] = nums[0]
-    dp[1] = Math.max(nums[0], nums[1])
-    for(let i=2; i<nums.length; i++) {
-        dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i])
-    }
-    return dp[nums.length-1]
+  let dp = [];
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+  for (let i = 2; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+  }
+  return dp[nums.length - 1];
 }
 
 // TC - O(N)
 // SC - O(1)
-function spaceOptimised(nums){
-    if(nums.length===1) return nums[0]
-    let dp0 = nums[0]
-    let dp1 = Math.max(nums[0], nums[1])
-    for(let i=2; i<nums.length; i++) {
-        const temp = Math.max(dp1, dp0+nums[i])
-        dp0 = dp1
-        dp1 = temp
-    }
-    return dp1
+function spaceOptimised(nums) {
+  if (nums.length === 1) return nums[0];
+  let dp0 = nums[0];
+  let dp1 = Math.max(nums[0], nums[1]);
+  for (let i = 2; i < nums.length; i++) {
+    const temp = Math.max(dp1, dp0 + nums[i]);
+    dp0 = dp1;
+    dp1 = temp;
+  }
+  return dp1;
 }
 
 // TC - O(N)
 // SC - O(N), for recursion stack and dp
 function recursive(nums, pos, dp) {
-    if(dp[pos]!==undefined) return dp[pos]
-    if(pos>=nums.length) return 0
-    return dp[pos] = Math.max(recursive(nums, pos+1, dp), nums[pos] + recursive(nums, pos+2, dp))
+  if (dp[pos] !== undefined) return dp[pos];
+  if (pos >= nums.length) return 0;
+  return (dp[pos] = Math.max(
+    recursive(nums, pos + 1, dp),
+    nums[pos] + recursive(nums, pos + 2, dp)
+  ));
 }
 
-var solveRecursive = function(nums) {
-    let dp = []
-    return recursive(nums, 0, dp)
+var solveRecursive = function (nums) {
+  let dp = [];
+  return recursive(nums, 0, dp);
 };
 
 // const nums = [1,2,3,1]
