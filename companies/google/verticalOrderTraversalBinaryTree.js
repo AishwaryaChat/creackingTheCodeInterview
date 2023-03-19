9f0c503a2a852d400c96a4cbf70b1075530358e0
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-03-19 11:10:15 +0530

    Vertical order traversal of binary tree

---
+++ companies/google/verticalOrderTraversalBinaryTree.js
@@ -0,0 +1,91 @@
+// Vertical Order Traversal of a Binary Tree
+// Hard
+// company
+// Facebook
+// Amazon
+// Bloomberg
+// Google
+// Given the root of a binary tree, calculate the vertical order traversal of the binary tree.
+
+// For each node at position (row, col), its left and right children will be at positions (row + 1, col - 1) and (row + 1, col + 1) respectively. The root of the tree is at (0, 0).
+
+// The vertical order traversal of a binary tree is a list of top-to-bottom orderings for each column index starting from the leftmost column and ending on the rightmost column. There may be multiple nodes in the same row and same column. In such a case, sort these nodes by their values.
+
+// Return the vertical order traversal of the binary tree.
+
+// Example 1:
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[9],[3,15],[20],[7]]
+// Explanation:
+// Column -1: Only node 9 is in this column.
+// Column 0: Nodes 3 and 15 are in this column in that order from top to bottom.
+// Column 1: Only node 20 is in this column.
+// Column 2: Only node 7 is in this column.
+// Example 2:
+
+// Input: root = [1,2,3,4,5,6,7]
+// Output: [[4],[2],[1,5,6],[3],[7]]
+// Explanation:
+// Column -2: Only node 4 is in this column.
+// Column -1: Only node 2 is in this column.
+// Column 0: Nodes 1, 5, and 6 are in this column.
+//           1 is at the top, so it comes first.
+//           5 and 6 are at the same position (2, 0), so we order them by their value, 5 before 6.
+// Column 1: Only node 3 is in this column.
+// Column 2: Only node 7 is in this column.
+// Example 3:
+
+// Input: root = [1,2,3,4,6,5,7]
+// Output: [[4],[2],[1,5,6],[3],[7]]
+// Explanation:
+// This case is the exact same as example 2, but with nodes 5 and 6 swapped.
+// Note that the solution remains the same since 5 and 6 are in the same location and should be ordered by their values.
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 1000].
+// 0 <= Node.val <= 1000
+
+// TC - O(N.log(N/k)), where N is number of nodes and k is width of the tree
+// SC - O(N)
+function solve(root) {
+  if (root === null) return [];
+  let queue = new Queue();
+  let map = {};
+  let min = Number.MAX_SAFE_INTEGER;
+  let max = Number.MIN_SAFE_INTEGER;
+  queue.enqueue([0, 0, root]);
+  while (!queue.isEmpty()) {
+    const [row, col, node] = queue.dequeue();
+    min = Math.min(min, col);
+    max = Math.max(max, col);
+    if (!map[col]) map[col] = [];
+    map[col].push([row, node.val]);
+    if (node.left) queue.enqueue([row + 1, col - 1, node.left]);
+    if (node.right) queue.enqueue([row + 1, col + 1, node.right]);
+  }
+  let result = [];
+  for (let i = min; i <= max; i++) {
+    if (map[i]) {
+      // Sorting because we want same level and same col nodes to be sorted by value
+      map[i].sort((a, b) => {
+        if (a[0] === b[0]) return a[1] - b[1];
+        else return a[0] - b[0];
+      });
+      result.push(map[i].map((a) => a[1]));
+    }
+  }
+  return result;
+}
+
+// const root = [3,9,20,null,null,15,7]
+// Output: [[9],[3,15],[20],[7]]
+
+// const root = [1,2,3,4,5,6,7]
+// Output: [[4],[2],[1,5,6],[3],[7]]
+
+// const root = [1,2,3,4,6,5,7]
+// Output: [[4],[2],[1,5,6],[3],[7]]
+
+console.log(solve())
