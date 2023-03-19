e854448718052733af975adad0b85949a2a284f7
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-03-19 11:53:47 +0530

    Solution for topk frequent elements

---
+++ companies/google/topKFrequentWords.js
@@ -0,0 +1,83 @@
+// Top K Frequent Words
+// Medium
+// company
+// Amazon
+// Bloomberg
+// Google
+// Given an array of strings words and an integer k, return the k most frequent strings.
+
+// Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
+
+// Example 1:
+
+// Input: words = ["i","love","leetcode","i","love","coding"], k = 2
+// Output: ["i","love"]
+// Explanation: "i" and "love" are the two most frequent words.
+// Note that "i" comes before "love" due to a lower alphabetical order.
+// Example 2:
+
+// Input: words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4
+// Output: ["the","is","sunny","day"]
+// Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the number of occurrence being 4, 3, 2 and 1 respectively.
+
+// Constraints:
+
+// 1 <= words.length <= 500
+// 1 <= words[i].length <= 10
+// words[i] consists of lowercase English letters.
+// k is in the range [1, The number of unique words[i]]
+
+// Follow-up: Could you solve it in O(n log(k)) time and O(n) extra space?
+
+const Heap = require("../../PracticeQuestions/Heaps/heapGeneralisedImplementation");
+
+// Can achive better TC - O(N), using bucket sort
+// TC - O(Nlogk)
+// SC - O(N)
+function solve() {
+  let minHeap = new Heap({
+    comparator: (a, b) => {
+      if (a[1] === b[1]) {
+        return a[0] > b[0];
+      }
+      return a[1] < b[1];
+    },
+  });
+  let map = {};
+  for (let word of words) {
+    if (!map[word]) map[word] = 0;
+    map[word] += 1;
+  }
+  const keys = Object.keys(map);
+  minHeap.push([keys[0], map[keys[0]]]);
+  for (let i = 1; i < keys.length; i++) {
+    const key = keys[i];
+    const value = map[key];
+    if (minHeap.getSize() >= k) {
+      if (value === minHeap.peek()[1]) {
+        if (key < minHeap.peek()[0]) {
+          minHeap.pop();
+          minHeap.push([key, value]);
+        }
+      } else if (value > minHeap.peek()[1]) {
+        minHeap.pop();
+        minHeap.push([key, value]);
+      }
+    } else minHeap.push([key, value]);
+  }
+  let result = [];
+  while (minHeap.getSize() > 0) {
+    result.push(minHeap.pop()[0]);
+  }
+  return result.reverse();
+}
+
+const words = ["i","love","leetcode","i","love","coding"]
+const k = 2
+// Output: ["i","love"]
+
+// const words = ["the","day","is","sunny","the","the","the","sunny","is","is"]
+// const k = 4
+// Output: ["the","is","sunny","day"]
+
+console.log(solve(words, k));
