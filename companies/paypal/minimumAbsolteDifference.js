289d08e4b933042b3c0925bbdf5ec40c7f067a1b
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-04-03 22:44:17 +0530

    Solution for minimum absolute difference

---
+++ companies/paypal/minimumAbsolteDifference.js
@@ -0,0 +1,79 @@
+// Minimum Absolute Difference
+// Easy
+// company
+// Paypal
+// Bloomberg
+// JPMorgan
+// Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
+
+// Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+
+// a, b are from arr
+// a < b
+// b - a equals to the minimum absolute difference of any two elements in arr
+
+// Example 1:
+
+// Input: arr = [4,2,1,3]
+// Output: [[1,2],[2,3],[3,4]]
+// Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+// Example 2:
+
+// Input: arr = [1,3,6,10,15]
+// Output: [[1,3]]
+// Example 3:
+
+// Input: arr = [3,8,-10,23,19,-4,-14,27]
+// Output: [[-14,-10],[19,23],[23,27]]
+
+// Constraints:
+
+// 2 <= arr.length <= 10^5
+// -10^6 <= arr[i] <= 10^6
+
+// TC - O(nlogn)
+function solve(arr) {
+  arr.sort((a, b) => a - b);
+  let min = Number.MAX_SAFE_INTEGER;
+  for (let i = 1; i < arr.length; i++) {
+    const diff = Math.abs(arr[i] - arr[i - 1]);
+    if (diff < min) {
+      min = diff;
+    }
+  }
+  let ans = [];
+  for (let i = 1; i < arr.length; i++) {
+    const diff = Math.abs(arr[i] - arr[i - 1]);
+    if (diff === min) ans.push([arr[i - 1], arr[i]]);
+  }
+  return ans;
+}
+
+// TC - O(nlogn)
+function solveOptimised(arr) {
+  arr.sort((a, b) => a - b);
+  let min = Number.MAX_SAFE_INTEGER;
+  let ans = [];
+  for (let i = 1; i < arr.length; i++) {
+    const diff = Math.abs(arr[i] - arr[i - 1]);
+    if (diff < min) {
+      min = diff;
+      ans = [];
+      ans.push([arr[i - 1], arr[i]]);
+    } else if (diff === min) {
+      ans.push([arr[i - 1], arr[i]]);
+    } 
+  }
+  return ans;
+}
+
+// const arr = [4, 2, 1, 3];
+// Output: [[1,2],[2,3],[3,4]]
+
+// const arr = [1,3,6,10,15]
+// Output: [[1,3]]
+
+const arr = [3, 8, -10, 23, 19, -4, -14, 27];
+// Output: [[-14,-10],[19,23],[23,27]]
+
+console.log(solve(arr));
