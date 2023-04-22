97d94e6dd064e14cc36854b6306b68c7ec619aba
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-04-22 17:08:22 +0530

    Solution to find valid sudoku

---
+++ Neetcode/ArraysAndHashing/validSudoku.js
@@ -0,0 +1,106 @@
+// Valid Sudoku
+// Medium
+// company
+// Amazon
+// Apple
+// Karat
+// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+
+// Example 1:
+
+// Input: board =
+// [["5","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]]
+// Output: true
+// Example 2:
+
+// Input: board =
+// [["8","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]]
+// Output: false
+// Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+// Constraints:
+
+// board.length == 9
+// board[i].length == 9
+// board[i][j] is a digit 1-9 or '.'.
+
+// TC - O(N^2)
+// SC - O(N^2)
+function isValidSquare(map, i, j, box, character) {
+  if (map.rows[i][character]) return false;
+  if (map.cols[j][character]) return false;
+  if (map.boxes[box][character]) return false;
+  return true;
+}
+
+function solve(board) {
+  let map = { rows: {}, cols: {}, boxes: {} };
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const character = board[i][j];
+      if (character !== ".") {
+        const boxId = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+        if (!map.rows[i]) map.rows[i] = {};
+        if (!map.cols[j]) map.cols[j] = {};
+        if (!map.boxes[boxId]) map.boxes[box] = {};
+        if (!isValidSquare(map, i, j, boxId, character)) return false;
+        map.rows[i][character] = true;
+        map.cols[j][character] = true;
+        map.boxes[boxId][character] = true;
+      }
+    }
+  }
+  return true;
+}
+
+const board = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+];
+// Output: true
+
+// const board = [
+//   ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+//   ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+//   [".", "9", "8", ".", ".", ".", ".", "6", "."],
+//   ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+//   ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+//   ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+//   [".", "6", ".", ".", ".", ".", "2", "8", "."],
+//   [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+//   [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+// ];
+// Output: false
+
+console.log(solve(board));
