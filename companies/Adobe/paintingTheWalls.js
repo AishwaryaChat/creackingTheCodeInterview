7eaa92a838a8c0b46cab4bf1173eba1d600bdafd
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-07-12 20:14:25 +0530

    Solution to paint walls

---
+++ companies/Adobe/paintingTheWalls.js
@@ -0,0 +1,65 @@
+// Painting the Walls
+// Hard
+// company
+// Amazon
+// Adobe
+// You are given two 0-indexed integer arrays, cost and time, of size n representing the costs and the time taken to paint n different walls respectively. There are two painters available:
+
+// A paid painter that paints the ith wall in time[i] units of time and takes cost[i] units of money.
+// A free painter that paints any wall in 1 unit of time at a cost of 0. But the free painter can only be used if the paid painter is already occupied.
+// Return the minimum amount of money required to paint the n walls.
+
+// Example 1:
+
+// Input: cost = [1,2,3,2], time = [1,2,3,2]
+// Output: 3
+// Explanation: The walls at index 0 and 1 will be painted by the paid painter, and it will take 3 units of time; meanwhile, the free painter will paint the walls at index 2 and 3, free of cost in 2 units of time. Thus, the total cost is 1 + 2 = 3.
+// Example 2:
+
+// Input: cost = [2,3,4,2], time = [1,1,1,1]
+// Output: 4
+// Explanation: The walls at index 0 and 3 will be painted by the paid painter, and it will take 2 units of time; meanwhile, the free painter will paint the walls at index 1 and 2, free of cost in 2 units of time. Thus, the total cost is 2 + 2 = 4.
+
+// Constraints:
+
+// 1 <= cost.length <= 500
+// cost.length == time.length
+// 1 <= cost[i] <= 10^6
+// 1 <= time[i] <= 500
+
+function solve(cost, time) {
+  const dp = {};
+  function findMinCost(pos, paintedCount, timeTaken) {
+    const key = pos + "_" + (paintedCount + timeTaken);
+    if (dp[key] !== undefined) return dp[key];
+    let output;
+    let res1, res2;
+    if (paintedCount + timeTaken >= cost.length) output = 0;
+    else {
+      if (pos === cost.length) output = -1;
+      else {
+        res1 = findMinCost(pos + 1, paintedCount + 1, timeTaken + time[pos]);
+        if (res1 !== -1) {
+          res1 += cost[pos];
+        }
+        res2 = findMinCost(pos + 1, paintedCount, timeTaken);
+        if (res1 === -1 && res2 === -1) output = -1;
+        else if (res1 === -1) output = res2;
+        else if (res2 === -1) output = res1;
+        else output = Math.min(res1, res2);
+      }
+    }
+    return (dp[key] = output);
+  }
+  return findMinCost(0, 0, 0, cost, time, dp);
+}
+
+// const cost = [1,2,3,2]
+// const time = [1,2,3,2]
+// Output: 3
+
+const cost = [2, 3, 4, 2];
+const time = [1, 1, 1, 1];
+// Output: 4
+
+console.log(solve(cost, time));
