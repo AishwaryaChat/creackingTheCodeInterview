77dd3b429dbb3fd1e31797429dcb606d8a264462
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-08-14 15:43:21 +0530

    Solution for topk frequent words

--- companies/google/topKFrequentWords.js
+++ companies/google/topKFrequentWords.js
@@ -34,7 +34,7 @@ const Heap = require("../../PracticeQuestions/Heaps/heapGeneralisedImplementatio
 // Can achive better TC - O(N), using bucket sort
 // TC - O(Nlogk)
 // SC - O(N)
-function solve() {
+function solve(words) {
   let minHeap = new Heap({
     comparator: (a, b) => {
       if (a[1] === b[1]) {
@@ -72,12 +72,89 @@ function solve() {
   return result.reverse();
 }
 
-const words = ["i","love","leetcode","i","love","coding"]
-const k = 2
+// Let N be the length of words.
+
+// Time Complexity: O(N). We take O(N)time to count frequencies and enumerate all buckets. Since we only need to get k words from tries, we traverse k paths in tries, and each path is neglectable in length (≤10), O(k)time is required to generate all those words from tries. Besides, it takes O(N) time to put N words in tries. As k≤N, O(N+k)=O(N)
+// Space Complexity: O(N), like other approaches, our counter cnt needs O(N) space. Besides, tries to store at most N words also need O(n) space.
+class Trie {
+  constructor() {
+    this.children = new Array(26);
+    this.end = false;
+  }
+  insert(word) {
+    let curr = this;
+    for (let letter of word) {
+      const charIndex = letter.charCodeAt(0) - 97;
+      if (curr.children[charIndex] === undefined)
+        curr.children[charIndex] = new Trie();
+      curr = curr.children[charIndex];
+    }
+    curr.end = true;
+  }
+  getTrieWords(trie, k, word, words) {
+    if (trie.end) {
+      words.push(word);
+      k.k -= 1;
+    }
+    for (let i = 0; i < trie.children.length && k.k > 0; i++) {
+      const letter = String.fromCharCode(i + 97);
+      if (trie.children[i]) {
+        this.getTrieWords(trie.children[i], k, `${word}${letter}`, words);
+      }
+    }
+    return words;
+  }
+
+  getWords(trie, k) {
+    return this.getTrieWords(trie, { k }, "", []);
+  }
+}
+
+function solveUsingTrieAndBucketSort(words, k) {
+  let map = {};
+  for (let word of words) {
+    if (!map[word]) map[word] = 0;
+    map[word] += 1;
+  }
+  let bucket = new Array(words.length + 1).fill().map(() => new Trie());
+  const keys = Object.keys(map);
+  for (let i = 0; i < keys.length; i++) {
+    const value = map[keys[i]];
+    const bucketTrie = bucket[value];
+    bucketTrie.insert(keys[i]);
+  }
+  let ans = [];
+  for (let i = bucket.length - 1; i >= 0 && k > 0; i--) {
+    const words = bucket[i].getWords(bucket[i], k);
+    k -= words.length;
+    ans = ans.concat(words);
+  }
+  return ans;
+}
+
+// const words = ["i","love","leetcode","i","love","coding"]
+// const k = 2
 // Output: ["i","love"]
 
-// const words = ["the","day","is","sunny","the","the","the","sunny","is","is"]
-// const k = 4
+// const words = [
+//   "the",
+//   "day",
+//   "is",
+//   "sunny",
+//   "the",
+//   "the",
+//   "the",
+//   "sunny",
+//   "is",
+//   "is",
+// ];
+// const k = 4;
 // Output: ["the","is","sunny","day"]
 
-console.log(solve(words, k));
+// const words = ["i","love","leetcode","i","love","coding"]
+// const k = 3
+
+const words = ["a", "aa", "aaa"];
+const k = 2;
+
+console.log(solveUsingTrieAndBucketSort(words, k));
