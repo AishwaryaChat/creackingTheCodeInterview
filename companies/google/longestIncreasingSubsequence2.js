e950a97af86060985000644597fb9adc054c0b5c
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-08-18 17:22:02 +0530

    Solution for finding longest inreasing subsequence

--- companies/google/longestIncreasingSubsequence2.js
+++ companies/google/longestIncreasingSubsequence2.js
@@ -42,12 +42,15 @@
 // 1 <= nums.length <= 10^5
 // 1 <= nums[i], k <= 10^5
 
+// TLE
+// TC - O(N^2)
+// SC - O(N)
 function dfs(pos, nums, k, dp) {
   if (pos === nums.length - 1) return 0;
   if (dp[pos] !== undefined) return dp[pos];
   let ans = 1;
   for (let i = pos - 1; i >= 0; i--) {
-    if (nums[pos]>nums[i] && nums[pos]-nums[i] <= k) {
+    if (nums[pos] > nums[i] && nums[pos] - nums[i] <= k) {
       ans = Math.max(ans, 1 + dfs(i, nums, k, dp));
     }
   }
@@ -57,14 +60,89 @@ function dfs(pos, nums, k, dp) {
 function solve(nums, k) {
   let dp = {};
   let ans = 1;
-  for (let i = nums.length-1; i >=0; i--) {
+  for (let i = nums.length - 1; i >= 0; i--) {
     ans = Math.max(ans, dfs(i, nums, k, dp));
   }
   return ans;
 }
 
-// const nums = [4, 2, 1, 4, 3, 4, 5, 8, 9];
-// const k = 3;
+// Solution using segment tree
+// TC - O(n log m) where n is number of elements in arrays list
+// SC - O(m) where m is maximum number in given array list
+// The logic here is to find the maximum length of subsequence till a given number, we were doing a linear search in previous approach and hence O(n^2) TC
+// Here we are using a segment tree to save this maxLength
+// The max value of segment tree is the maximum value of elements in given array list
+// searching the segment tree will take log n time, and hence O(nlog m) complexity
+class Node {
+  constructor(num, l, r, maxLength = 0) {
+    this.num = num;
+    this.l = l;
+    this.r = r;
+    this.left = null;
+    this.right = null;
+    this.maxLength = maxLength;
+  }
+}
+
+class SegmentTree {
+  constructor(nums) {
+    this.segmentTree = this.buildTree(0, Math.max(...nums));
+  }
+  buildTree(min, max) {
+    if (min === max) {
+      return new Node(min, min, min);
+    }
+    const mid =  Math.floor((max + min) / 2);
+    const newNode = new Node(mid, min, max);
+    newNode.left = this.buildTree(min, mid);
+    newNode.right = this.buildTree(mid + 1, max);
+    return newNode;
+  }
+
+  update(num, maxLength, root = this.segmentTree) {
+    if (num === root.l && num === root.r) {
+      root.maxLength = maxLength;
+      return;
+    }
+    const mid =  Math.floor((root.r + root.l) / 2);
+    if (num > mid) {
+      this.update(num, maxLength, root.right);
+    } else {
+      this.update(num, maxLength, root.left);
+    }
+    root.maxLength = Math.max(root.left.maxLength, root.right.maxLength);
+  }
+
+  query(min, max, root = this.segmentTree) {
+    if (min === root.l && max === root.r) return root.maxLength;
+    if ((min > root.r || max < root.l)) return 0;
+    const mid =  Math.floor((root.l + root.r) / 2);
+    if (min > mid) {
+      return this.query(min, max, root.right);
+    } else if (max <= mid) {
+      return this.query(min, max, root.left);
+    } else {
+      return Math.max(
+        this.query(min, mid, root.left),
+        this.query(mid + 1, max, root.right)
+      );
+    }
+  }
+}
+
+function solveOptimised(nums, k) {
+  const segmentTree = new SegmentTree(nums);
+  let ans = 0;
+  for (let num of nums) {
+    const length = 1 + segmentTree.query(Math.max(num - k, 0), num - 1);
+    segmentTree.update(num, length);
+    ans = Math.max(ans, length);
+  }
+  return ans
+}
+
+const nums = [4, 2, 1, 4, 3, 4, 5, 8, 9];
+const k = 3;
 // Output: 5
 
 // const nums = [7,4,5,1,8,12,4,7]
@@ -78,7 +156,7 @@ function solve(nums, k) {
 // const nums = [1,10000]
 // const k = 10000
 
-const nums =[1,100,500,100000,100000]
-const k =100000
+// const nums =[1,100,500,100000,100000]
+// const k =100000
 
-console.log(solve(nums, k));
+console.log(solveOptimised(nums, k));
